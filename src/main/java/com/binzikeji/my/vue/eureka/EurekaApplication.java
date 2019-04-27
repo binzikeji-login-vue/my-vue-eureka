db531cbeb03592b770b0376b908855cc1e8d8e8c
ShuaiBin <3166688833@qq.com>
2019-04-27 18:01:49 +0800

    update

---
+++ src/main/java/com/binzikeji/my/vue/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.binzikeji.my.vue.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description
+ * @Author Bin
+ * @Date 2019/4/27 15:48
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
